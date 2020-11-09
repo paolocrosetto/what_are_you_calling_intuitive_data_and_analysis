dfdab624a199cfff73f42a03d1ac35c484bac617
paolocrosetto <paolo.crosetto@gmail.com>
2020-11-09 10:44:10 +0100

    Figure 6

---
+++ respondent_transition_small_large.R
@@ -0,0 +1,20 @@
+# Figure 6
+
+df %>% 
+  # compute % of acceptances by subject
+  filter(offer == 1) %>%
+  group_by(subject_ID,pie_size, subject_type) %>%
+  summarise(prob_accept = mean(reaction == "Accept")) %>% 
+  # preparing data for plot
+  group_by(subject_ID,  subject_type, pie_size) %>% 
+  pivot_wider(names_from = pie_size, values_from = prob_accept) %>% 
+  # plot
+  ggplot(aes(`11`, `19`, fill = subject_type))+
+  geom_point(pch = 21, size = 5, alpha = 0.8, color = "black")+
+  geom_abline(slope = 1, linetype = "dashed")+
+  scale_fill_manual(name = "", values = c("grey99", "grey33","grey1", "grey66"))+
+  labs(x = "Probability of accepting a 1-euro offer from a pie of 11",
+       y = "Probability of accepting a 1-euro offer from a pie of 19")
+
+# save the figure
+ggsave("Figures/Figure_6.png", width = 8, height = 5, units = "in")
