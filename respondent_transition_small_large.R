bc71278b652ad5399f7b6628e97ba3ed18644a0e
paolocrosetto <paolo.crosetto@gmail.com>
2020-11-10 17:35:50 +0100

    test of prob of accept by pie size

--- respondent_transition_small_large.R
+++ respondent_transition_small_large.R
@@ -19,3 +19,15 @@ df %>%
 
 # save the figure
 ggsave("Figures/Figure_6.png", width = 8, height = 5, units = "in")
+
+
+## tests to show different behavior of types in the transition from small to large pie
+df %>% 
+  # compute % of acceptances by subject
+  filter(offer == 1) %>%
+  group_by(subject_ID,pie_size, subject_type) %>%
+  summarise(prob_accept = mean(reaction == "Accept")) %>% 
+  # test if acceptance rate is different 
+  group_by(subject_type) %>% 
+  group_modify(~tidy(wilcox.test(prob_accept~pie_size, data= .)))
+
