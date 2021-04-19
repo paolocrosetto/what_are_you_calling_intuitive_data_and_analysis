cb3028feff5f892b44fd7853806507650898fdb5
paolocrosetto <paolo.crosetto@gmail.com>
2021-04-19 16:00:24 +0200

    random effect panel regression for response time

--- Subfiles/respondent_self_prime.R
+++ Subfiles/respondent_self_prime.R
@@ -3,20 +3,20 @@
 # ratio of acceptances by subject -- 1-euro offers
 acceptance_ratio <- df %>% 
   filter(offer == 1) %>%
-  group_by(subject_ID,pie_size, offer) %>%
+  group_by(subject_ID,pie_size) %>%
   summarise(prob_accept = mean(reaction == "Accept"))
 
 # reaction time -- 1-euro offers
 reaction_time <- df %>% 
   filter(offer == 1) %>% 
-  select(subject_ID, pie_size, offer, reaction_time, reaction) %>% 
-  group_by(subject_ID, pie_size, offer, reaction) %>% 
+  select(subject_ID, pie_size, reaction_time, reaction) %>% 
+  group_by(subject_ID, pie_size, reaction) %>% 
   filter(reaction != "Error") %>% 
   summarise(time = mean(reaction_time, na.rm = T))
 
 # merging
 reaction_and_acceptance <- reaction_time %>% 
-  left_join(acceptance_ratio, by = c("subject_ID", "pie_size", "offer")) %>% 
+  left_join(acceptance_ratio, by = c("subject_ID", "pie_size")) %>% 
   mutate(reaction = fct_relevel(reaction, "Accept"))
 
 # plot
@@ -38,33 +38,67 @@ reaction_and_acceptance %>%
 # saving the plot
 ggsave("Figures/Figure_7.png", width = 8, height = 5, units = "in")
 
-#cleaning
+
+## Panel regression
+
+# focus on data for 1-€ offers
+regdf <- df %>% 
+  filter(offer == "1") %>% 
+  filter(reaction != "Error") %>% 
+  select(subject_ID, choice_N, everything())
 
 
-## Table 3 
+# merge prob_accpet with data
+regdf <- regdf %>% 
+  left_join(acceptance_ratio, by = c("subject_ID", "pie_size"))
 
-# column 1: pie size 11
-fit11 <- reaction_and_acceptance %>%  
-  filter(pie_size == 11) %>% 
-  lm(time~prob_accept*reaction, data= .)
 
-# column 2: pie size 19
-fit19 <- reaction_and_acceptance %>%  
-  filter(pie_size == 19) %>% 
-  lm(time~prob_accept*reaction, data= .)
+# we regress the reaction time on
+# the reaction (time-variant) interacted with the share of acceptances of each subject (time-invariant)
+# there is within-subject variability, so we opt for a random effect model
 
-# export to csv file
-tidy(fit11) %>% 
-  mutate(column = "pie = 11") %>% 
-  bind_rows(tidy(fit19) %>% mutate(column = "pie = 19")) %>% 
-  select(column, everything()) %>% 
-  write_csv("Tables/Table_3.csv")
+# handling panel data with the plm library
+library(plm)
 
-# export to latex
+## pie size of 11
+
+# RE
+random_effect_est_11 <- plm(reaction_time ~ prob_accept*reaction, data = regdf %>% filter(pie_size == 11), effect = "individual", model = "random") 
+
+# FE
+fixed_effect_est_11 <- plm(reaction_time ~ prob_accept*reaction, data = regdf %>% filter(pie_size == 11), model = "within") 
+
+# test indicates we should prefer RE (p-value 0.8414)
+phtest(random_effect_est_11, fixed_effect_est_11)
+
+# clustered standard errors
+
+ref_11_cluster <- coeftest(random_effect_est_11, vcov=vcovHC(ref,type="HC0",cluster="group"))
+
+
+# pie size of 19
+# RE
+random_effect_est_19 <- plm(reaction_time ~ prob_accept*reaction, data = regdf %>% filter(pie_size == 19), effect = "individual", model = "random") 
+
+# FE
+fixed_effect_est_19 <- plm(reaction_time ~ prob_accept*reaction, data = regdf %>% filter(pie_size == 19), model = "within") 
+
+# test indicates we should prefer RE (p-value 0.7934)
+phtest(random_effect_est_19, fixed_effect_est_19)
+
+# clustered standard errors
+ref_19_cluster <- coeftest(random_effect_est_19, vcov=vcovHC(ref,type="HC0",cluster="group"))
+
+
+# output to latex -- tweaked manually for the paper but all is here
 library(stargazer)
-stargazer(fit11, fit19, title = "Response time as a function of the frequency of acceptance -- offers of 1", 
-          column.labels = c("Pie size 11", "Pie size 19"))
+stargazer(random_effect_est_11, ref_11_cluster, random_effect_est_19, ref_19_cluster,
+          title = "Response time as a function of the frequency of acceptance -- offers of 1", 
+          column.labels = c("Pie size 11", "clust SE", "Pie size 19", "clust SE"))
 
-
-rm(acceptance_ratio, reaction_time, reaction_and_acceptance, fit11, fit19)
+# cleaning up
+rm(acceptance_ratio, reaction_time, reaction_and_acceptance, 
+   random_effect_est_11, random_effect_est_19,
+   fixed_effect_est_11, fixed_effect_est_19,
+   ref_11_cluster, ref_19_cluster)
 
